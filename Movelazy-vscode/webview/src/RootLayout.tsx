6aa2a3b22b21c477699f9b663164219cdca60a9d
Menhythien <dinhthienmenh1505@gmail.com>
2024-10-28 23:38:11 +0700

    #5 redefind main layout

--- Movelazy-vscode/webview/src/RootLayout.tsx
+++ Movelazy-vscode/webview/src/RootLayout.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
-import { Feedback } from "./components/Feedback";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-export const RootLayout = () => {
+export const RootLayout: React.FC = () => {
   return (
     <div className="relative bg-[#0e0f0e] flex flex-col justify-between w-full min-h-screen">
-      <Outlet />
-      <Feedback />
+      {/* Header có thể thêm vào đây nếu cần */}
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      {/* Footer có thể thêm vào đây nếu cần */}
     </div>
   );
 };
