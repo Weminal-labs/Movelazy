e62f6507dcdbbef723c88072e82c56622bff3962
Menhythien <dinhthienmenh1505@gmail.com>
2024-11-01 01:06:25 +0700

    update webview panel

--- src/MovelazyViewProvider.ts
+++ src/MovelazyViewProvider.ts
@@ -10,29 +10,37 @@ export class MovelazyViewProvider implements vscode.WebviewViewProvider {
     public resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken) {
         webviewView.webview.options = {
             enableScripts: true,
-            localResourceRoots: [this._context.extensionUri] // Đảm bảo webview có thể tải tài nguyên cục bộ
+            localResourceRoots: [
+                vscode.Uri.joinPath(this._context.extensionUri, 'webview', 'build')
+            ]
         };
 
         webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
     }
 
-    public _getHtmlForWebview(webview: vscode.Webview) {
+    private _getHtmlForWebview(webview: vscode.Webview) {
         const scriptUri = webview.asWebviewUri(
-            vscode.Uri.file(path.join(this._context.extensionPath, 'webview', "build", 'assets', 'index.js'))
+            vscode.Uri.joinPath(this._context.extensionUri, 'webview', 'build', 'assets', 'index.js')
         );
         const styleUri = webview.asWebviewUri(
-            vscode.Uri.file(path.join(this._context.extensionPath, 'webview', "build", 'assets', 'index.css'))
+            vscode.Uri.joinPath(this._context.extensionUri, 'webview', 'build', 'assets', 'index.css')
         );
+        const logoUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._context.extensionUri, 'webview', 'build', 'assets', 'logo.svg')
+        );
+
         return `
-             <!DOCTYPE html>
+            <!DOCTYPE html>
             <html lang="en">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>My React App</title>
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; script-src ${webview.cspSource}; style-src ${webview.cspSource};">
+                <title>MoveLazy</title>
                 <link href="${styleUri}" rel="stylesheet">
             </head>
             <body>
+                <img src="${logoUri}" alt="MoveLazy Logo" />
                 <div id="root"></div>
                 <script src="${scriptUri}"></script>
             </body>
