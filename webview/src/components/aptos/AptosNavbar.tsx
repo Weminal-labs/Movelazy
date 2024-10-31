f74a87cd978bf561c08d790a6e969be0642547e3
Menhythien <dinhthienmenh1505@gmail.com>
2024-10-31 19:58:18 +0700

    navbar: add back button

--- webview/src/components/aptos/AptosNavbar.tsx
+++ webview/src/components/aptos/AptosNavbar.tsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { ArrowLeft } from '../../assets/icons/ArrowLeft/ArrowLeft'
 
 export const AptosNavbar = () => {
+  const navigate = useNavigate()
+
   return (
-    <nav className="p-4 border-b border-white/10">
+    <nav className="flex items-center justify-between p-4 border-b border-white/10">
+      <button
+        onClick={() => navigate('/')}
+        className="flex items-center gap-2 text-white hover:text-white/80"
+      >
+        <ArrowLeft className="w-5 h-5" />
+        <span>Back</span>
+      </button>
+
       <ul className="flex gap-4">
         <li>
           <Link to="/aptos/move" className="text-white hover:text-white/80">
