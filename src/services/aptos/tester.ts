c982ff47c5f7fa1e08c38597a9518fc654bfca1f
Khoa-Dam <damngockhoa0703@gmail.com>
2024-11-08 17:02:32 +0700

    #6 Custom network Movement

--- src/services/aptos/tester.ts
+++ src/services/aptos/tester.ts
@@ -1,61 +1,14 @@
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 import { promisify } from 'util';
+import path from 'path';
+import fs from 'fs';
+import yaml from 'js-yaml';
 
 const execAsync = promisify(exec);
 
 export class AptosTesterService {
-    async compile(webview: vscode.Webview, packageDir: string, namedAddresses: string, moveVersion: string, optimizer: boolean, optimizerlevel: string, bytecodeHash: string) {
-        const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-        if (!workspacePath) {
-            webview.postMessage({
-                type: 'compileStatus',
-                success: false,
-                message: 'No workspace folder found.'
-            });
-            return;
-        }
-
-        try {
-            // Check if Aptos CLI is installed
-            await execAsync('aptos --version', { cwd: workspacePath });
-            let command = `aptos move compile --package-dir ${packageDir} --named-addresses ${namedAddresses} `;
-
-            if (moveVersion === 'Move 2') {
-                command += `--move-2`;
-            } else {
-                command += `${optimizer === true ? `--optimize ${optimizerlevel}` : ''} --bytecode-version ${bytecodeHash} `;
-            }
-            const { stdout, stderr } = await execAsync(command, { cwd: workspacePath });
-
-
-            const isInformational = stdout.includes('"Result"');
-
-            if (stderr && !isInformational) {
-                webview.postMessage({
-                    type: 'compileStatus',
-                    success: false,
-                    message: stderr
-                });
-                return;
-            }
-
-            webview.postMessage({
-                type: 'compileStatus',
-                success: true,
-                message: stderr + stdout || 'Compilation and publishing successful!'
-            });
-
-        } catch (error) {
-            webview.postMessage({
-                type: 'compileStatus',
-                success: false,
-                message: (error as Error).message
-            });
-        }
-    }
-
-    async tester(webview: vscode.Webview, enabled: boolean, testName: string, moveVersion: string, optimizer: boolean, optimizerlevel: string, bytecodeHash: string) {
+    async tester(webview: vscode.Webview, enabled: boolean, testName: string, moveVersion: string, nameAddresses: string, optimizer: boolean, optimizerlevel: string, bytecodeHash: string) {
         const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
         if (!workspacePath) {
             webview.postMessage({
@@ -69,7 +22,36 @@ export class AptosTesterService {
         try {
             // Check if Aptos CLI is installed
             await execAsync('aptos --version', { cwd: workspacePath });
-            let command = `aptos move test ${enabled ? `-f ${testName}` : ''} `;
+
+            let accountAddress: string | null = null;
+            function readAccountFromConfig() {
+                if (!workspacePath) {
+                    console.error('Workspace path is undefined.');
+                    return;
+                }
+                const configFilePath = path.join(workspacePath, '.aptos', 'config.yaml'); // Đường dẫn đến file config.yaml
+
+                if (fs.existsSync(configFilePath)) {
+                    try {
+                        const fileContents = fs.readFileSync(configFilePath, 'utf8');
+                        const config: any = yaml.load(fileContents);
+
+                        if (config && config.profiles && config.profiles.default && config.profiles.default.account) {
+                            accountAddress = config.profiles.default.account;
+                            console.log('Account Address from config:', accountAddress);
+                        } else {
+                            console.error('Account not found in config file.');
+                        }
+                    } catch (error) {
+                        console.error('Error parsing config file:', error);
+                    }
+                } else {
+                    console.error(`Config file ${configFilePath} does not exist.`);
+                }
+            }
+
+            readAccountFromConfig();
+            let command = `aptos move test --named-addresses ${nameAddresses}=${accountAddress} ${enabled ? `-f ${testName}` : ''} `;
 
             if (moveVersion === 'Move 2') {
                 command += `--move-2`;
